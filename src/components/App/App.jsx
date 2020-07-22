7d87864a1acf3596828a238545ececfee1a3afd0
Alisher <aali@dar.kz>
2020-07-22 15:19:54 +0600

    refactor: page routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
+import { Route, HashRouter as Router } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 import SignIn from '../../containers/SignIn';
 import Dashboard from '../../containers/Dashboard';
 
-const isToken = localStorage.getItem('token');
+// const isToken = localStorage.getItem('token');
 export const App = () => {
-	function PrivateRoute({ children, ...rest }) {
-		return <Route {...rest} render={(props) => (isToken ? children : <Redirect to={{ pathname: '/bizkit' }} />)} />;
-	}
+	// function PrivateRoute({ children, ...rest }) {
+	// 	return <Route {...rest} render={(props) => (isToken ? children : <Redirect to={{ pathname: '/bizkit' }} />)} />;
+	// }
 
+	console.log(process.env.PUBLIC_URL);
 	return (
 		<div>
 			<Helmet>
 				<link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
 			</Helmet>
-			<Router>
-				<Route path='/bizkit' exact={true}>
+			<Router basename={process.env.PUBLIC_URL}>
+				<Route path='/' exact={true}>
 					<SignIn />
 				</Route>
-				<PrivateRoute path='/dashboard'>
+				<Route path='/dashboard' exact={true}>
 					<Dashboard />
-				</PrivateRoute>
+				</Route>
 			</Router>
 		</div>
 	);
